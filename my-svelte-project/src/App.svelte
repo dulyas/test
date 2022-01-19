a0fab2b396959a2270aba2498d70c288ca7c1dfc
Kirill <d.str1ke@yandex.ru>
2022-01-19 16:46:38 +0500

    first commit

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+import Block from "./Block.svelte";
+const blocks = [
+	{
+		color: 'red',
+		opacity: 1,
+	},
+	{
+		color: 'blue',
+		opacity: 1,
+	},
+	{
+		color: 'green',
+		opacity: 1,
+	},
+];
+
+function handleClick(i) {
+	if (blocks[i].opacity === 1) {
+		blocks.forEach((block) => block.opacity = 1);
+		blocks[i].opacity = 0;
+	} else if (blocks[i].opacity === 0) {
+		blocks[i].opacity = 1;
+	}
+}
+
+</script>
+
+<main>
+
+	{#each blocks as block, i}
+	<div on:click={() => handleClick(i)}>
+		<Block bgColor={block.color} opacity={block.opacity} />
+	</div>
+	{/each}
+
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		justify-content: space-around;
+		margin: 0 auto;
+		width: 1020px;
+	}
+</style>
---
+++ my-svelte-project/src/Block.svelte
@@ -0,0 +1,15 @@
+<script>
+export let bgColor;
+export let opacity;
+</script>
+
+<main style="background-color: {bgColor}; opacity: {opacity}">
+	
+</main>
+
+<style lang="scss">
+ main {
+     width: 100px;
+     height: 100px;
+ }
+</style>
